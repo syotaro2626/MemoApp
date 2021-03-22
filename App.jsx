e301c4ddafbf71dd2f38b04079fced4af67be409
高井将太 <takaishouta@MacBook-Pro.local>
2021-03-22 21:28:27 +0900

    Add Navigation

--- App.jsx
+++ App.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-// import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
-// import MemoCreateScreen from './src/screens/MemoCreateScreen';
-// import LogInScreen from './src/screens/LogInScreen';
-// import SignUpScreen from './src/screens/SignUpScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <MemoListScreen />
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="SignUp"
+        screenOptions={{
+          headerStyle: { backgroundColor: '#467FD3' },
+          headerTitleStyle: { color: '#ffffff' },
+          headerTitle: 'MemoApp',
+          headerTintColor: '#ffffff',
+          headerBackTitle: 'back',
+        }}
+      >
+        <Stack.Screen name="MemoList" component={MemoListScreen} />
+        <Stack.Screen name="MemoDetail" component={MemoDetailScreen} />
+        <Stack.Screen name="MemoEdit" component={MemoEditScreen} />
+        <Stack.Screen name="MemoCreate" component={MemoCreateScreen} />
+        <Stack.Screen name="LogIn" component={LogInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
